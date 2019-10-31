93b90172d4fed0b067c9ea3a4e6ca080ec8720e0
emisa182 <55809352+emisa182@users.noreply.github.com>
2019-10-31 22:28:41 +0100

    Update soundsplit.m

--- soundsplit.m
+++ soundsplit.m
@@ -1,3 +1,9 @@
-[y,Fs] = audioread('World_of_Goo.wav');    %Läser in ljudfilen
-matsize = Fs/50;                           %Bestämmer matrisstorleken(hur mycket vi delar upp ljudfilen)
-split_sound = vec2mat(y,matsize)';         %Gör om ljudfilen till mindre bitar av ljudfilen 
+[y,fs] = audioread('World_of_Goo.wav'); % Läser in ljudfilen
+L = length(y);                          % Storleken på ljudfilen
+T = 1/fs;                               % Sampling period
+t = (0:L-1)*T;                          % Time vector
+matsize = fs/50;                        % Bestämmer matrisstorleken(hur mycket vi delar upp ljudfilen)
+split_sound = vec2mat(y,matsize)';      % Gör om ljudfilen till mindre bitar av ljudfilen 
+fourier = fft(split_sound,[],1);        % Tar matrisen till frekvensdomänen
+%hitta ett sätt att gå från fourier tillbaka till ett förvrängt y
+plot (abs(fourier));                    %Nyquistfrekvens?
