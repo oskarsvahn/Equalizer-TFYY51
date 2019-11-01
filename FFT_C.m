cdc357ad3f8960f21d5f7d3e98bd67f3b183e5fa
Callesteen <55879715+Callesteen@users.noreply.github.com>
2019-11-01 10:36:33 +0100

    Update FFT_C.m

--- FFT_C.m
+++ FFT_C.m
@@ -1,1 +1,4 @@
-[y,Fs] = audioread('1.1World_of_Goo.wav'); %Läser ljudfil
+[y,Fs] = audioread('1.1World_of_Goo.wav');  %Läser ljudfil
+L = length(y);                              %Längden på y vektorn
+T = 1/Fs;                                   %Sampling period
+t = (0:L-1)*T;                              %Tids vektorn
