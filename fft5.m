a34348409c397025348add2d6a21afb0e57f2028
Oskar Svahn <eosksva001@utb.hoglandet.se>
2019-11-13 16:28:44 +0100

    Update fft5.m

--- fft5.m
+++ fft5.m
@@ -1,24 +1,32 @@
-filterValues=[0,0.7;
-              125,0.8;
-              500,1;
-              1000, 1.3;
-              2000,1.2;
+filterValues=[125,0;
+              250,0;
+              500,0.1;
+              1000,0.3;
+              2000,0.8;
+              3000,1;
               4000,1.5;
-              10000,1.2
-              20000, 1];
+              6000, 2;
+              8000,3;
+              12000,3;
+              20000,3];
           
-T=4          
+          
 x = filterValues(:,1); 
 y = filterValues(:,2);
 FFTLength=40000;
 
-Fs=44000;
+Fs=44100;
 xq = 1:1:FFTLength/2;
 
 
-filter =[];
+filter = []
+
 filter = interp1(x,y,xq, 'cubic');
-filter = [filter, flip(filter)]';
+
+plot(x,y,'o',xq,filter,':.');
+xlim([0 20000]);
+title('(Default) Linear Interpolation');
+filter = [fliplr(filter), filter ]';
 %recObj = audiorecorder(Fs, 8, 1)
 %recordblocking(recObj, T);
 %rawData = getaudiodata(recObj);%Inspelning
@@ -26,7 +34,8 @@ filter = [filter, flip(filter)]';
 
 frec = stft(rawData,Fs,'Window',hann(FFTLength,'periodic'),'OverlapLength',FFTLength/2,'FFTLength',FFTLength);
 frec2=frec.*filter;
-output=real(istft(frec2, Fs,'Window',hann(FFTLength,'periodic'),'OverlapLength',FFTLength/2,'FFTLength',FFTLength));
+complexOutput = istft(frec2, Fs,'Window',hann(FFTLength,'periodic'),'OverlapLength',FFTLength/2,'FFTLength',FFTLength);
+output=real(complexOutput)+imag(complexOutput);
 sound(output, Fs);
 
 
@@ -35,7 +44,3 @@ sound(output, Fs);
 
 
 
-filter = interp1(x,v,xq, 'cubic');
-plot(x,v,'o',xq,filter,':.');
-xlim([0 20000]);
-title('(Default) Linear Interpolation');
